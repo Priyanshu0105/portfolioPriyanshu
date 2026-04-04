46164052e0c4b729e7197bbf320eba5976ef3a7f
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-04-04 23:13:19 +0530

    Add Project and Stat interfaces for type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface Project {
+  num:  string;
+  name: string;
+  desc: string;
+  tags: string[];
+  url:  string;
+  year: string;
+}
+
+export interface Stat {
+  label: string;
+  value: string;
+}
